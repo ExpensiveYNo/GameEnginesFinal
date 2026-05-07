1219d46f1f22d676a90a3f13b89b9ffca9f8adbe
Max Stachura <maxstachura72005@gmail.com>
2026-05-07 15:21:08 -0500

    made key pickup script work with new input system and fixed spelling errors on some assets.

--- GameEnginesFinalProject/Assets/Final Prefab/OurScripts/key_pickup.cs
+++ GameEnginesFinalProject/Assets/Final Prefab/OurScripts/key_pickup.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class key_pickup : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class key_pickup : MonoBehaviour
 
     public AudioClip collectSound;
 
-    public GameObject collectEffect;
+    //public GameObject collectEffect;
 
     public bool flag = false, is_open = false;
 
@@ -30,14 +31,27 @@ public class key_pickup : MonoBehaviour
         if (rotate)
             transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
 
-        if (flag && Input.GetKeyDown(KeyCode.E))
+        if (flag && Keyboard.current.eKey.wasPressedThisFrame)
         {
-            AudioSource.PlayClipAtPoint(collectSound, transform.position);
+            if (collectSound != null)
+                AudioSource.PlayClipAtPoint(collectSound, transform.position);
             is_open = true;
-            KeyHubScript.instance.AddPoint();
-
+            //KeyHubScript.instance.AddPoint();
             Destroy(gameObject);
         }
     }
+
+    //Doesn't work because object is not child of player object 
+    //public void OnInteract(InputValue value)
+    //{
+    //    if (flag)
+    //    {
+    //        AudioSource.PlayClipAtPoint(collectSound, transform.position);
+    //        is_open = true;
+    //        //KeyHubScript.instance.AddPoint();
+
+    //        Destroy(gameObject);
+    //    }
+    //}
 
 }
